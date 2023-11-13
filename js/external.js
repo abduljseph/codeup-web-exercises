28780d0e76d20128118bda9e62be84d50e15f18f
Abdul Joseph <abduljseph@gmail.com>
2023-11-13 14:41:48 -0600

    added todays files

---
+++ js/external.js
@@ -0,0 +1,28 @@
+"use strict";
+
+console.log("Hello from external JavaScript");
+alert("Welcome to my Website!");
+let favoritecolor = prompt("What is your favorite color:")
+//let userInput= prompt("What is your favorite color?");
+//console.log("The user entered: ${userInput}");
+alert(`Great, ${favoritecolor} is my favorite color too!`);
+let littleMermaidRentedDays = prompt ("How many days did you rent Little Mermaid?");
+let brotherBearRentedDays  = prompt ("How many days did you rent Brother Bear?");
+let herculesRentedDays   = prompt ("How many days did you rent Hercules?");
+let costOfRentalPerDay = prompt("How much is the movie per day?")
+let totalRentalAmount=  rentalRate* littleMermaidRentedDays+rentalRate* brotherBearRentedDays+ rentalRate *herculesRentedDays  ;
+alert(`You spent$${totalRentalAmount.toFixed (2)}`) on rental moves this week');
+let googleHoursWorked = Number(prompt ("How many hours did you work for Google?");
+let amazonHoursWorked = Number(prompt( "How many hours did you work for Amazon?");
+let facebookHoursWorked = Number( (prompt( "How many hours did you work for Facebook");
+
+let googleHourlyrate Number(prompt("How much does google pay per hour?");
+let amazonHourlyrate Number(prompt("How much does amazon pay per hour?");
+let facebookHourlyrate Number(prompt("How much does facebook pay per hour?");
+
+
+
+console.log(costOfRentalPerDay);
+console.log(googlePaysPerHour);
+console.log(amazonPaysPerHour);
+console.log(facebookPaysPerHour);
