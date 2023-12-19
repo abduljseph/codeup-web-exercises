f2876136a023b54274bcda61b8300a4d0c0a52a4
Abdul Joseph <abduljseph@gmail.com>
2023-12-19 09:03:37 -0600

    created crc website shell

--- js/attributes-script.js
+++ js/attributes-script.js
@@ -1,20 +1,42 @@
-// setTimeout(function() {
-//     document.getElementById("profile-pic").setAttribute("src", "new_image.jpg");
-// }, 2000);
+console.log('Inside attributes-script.js');
 
-// setTimeout(function() {
-//     document.getElementById("profile-name").innerHTML = "New Name";
-// }, 4000); // 4000 milliseconds is 4 seconds
+// Global Variables
+const profilePicImg = document.querySelector('#profile-pic');
+const profileNameH2 = document.querySelector('#profile-name');
+const profileDescP = document.querySelector('#profile-desc');
+const profileCardDiv = document.querySelector('#profile-card');
 
-// // code below did not work
-// document.addEventListener("DOMContentLoaded", function() {
-//     setTimeout(function() {
-//         document.querySelector(".profile-name").innerHTML = "New Name";
-//     }, 4000);
-// });
 
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        document.querySelector("#profile-desc").classList.add("highlight-desc");
+// Functions
+function changeProfilePicSrc() {
+    const profilePicTimeoutId = setTimeout(() => {
+        profilePicImg.setAttribute('src', );
+    }, 2000);
+}
+function changeProfileName() {
+    const profileNameTimeoutId = setTimeout(() => {
+        profileNameH2.innerHTML = 'Steve Pflug';
+    }, 4000);
+}
+function changeProfileDesc() {
+    const profileDescTimeoutId = setTimeout(() => {
+        profileDescP.classList.add('desc-text-style');
     }, 6000);
-});
+}
+function changeCardBgColor() {
+    const profileCardIntervalId = setInterval(() => {
+        profileCardDiv.classList.toggle('pink-bg');
+    }, 2000);
+}
+
+
+// Events
+
+
+// On Load
+window.onload = () => {
+    changeProfilePicSrc();
+    changeProfileName();
+    changeProfileDesc();
+    changeCardBgColor();
+}
